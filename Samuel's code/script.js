0e00167da874efb7a461cd16180e424af4ecfaa5
SamuDev2024 <samuelgonzalezcoco@gmail.com>
2024-09-27 18:45:30 -0500

    Add files via upload

---
+++ Samuel's code/script.js
@@ -0,0 +1,31 @@
+/*clase sobre declaracion de variables
+hay distintos tipos de variables 
+var= variable global= se pueden usar en cualquier momento
+let=variable local=solo se pueden nombrar una vez
+const=son para informacion constante 
+se aconseja usar variables de tipo let 
+*/
+
+/*
+se utiliza el nombre de la variable para realizar una funcion 
+-se tiene que elegir el nombre de la clase donde se quiere la funcion 
+-console.log se usa para trabajar la consola 
+*/
+
+/*
+
+1.tipos de datos numericos 
+-enteros 
+-decimales
+-binarios
+
+2.texto
+-cadenas de texto = strings = se ponen entre "comillas" o 'comillas simples' 
+
+3.booeleanos
+-falso o verdadero= boolean = true/false 
+
+4.bloob
+-tiny/big 
+
+*/
